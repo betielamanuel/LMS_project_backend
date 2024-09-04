56409178346b3beaeb66aed004b25da8990f721a
Betiel Amanuel <betiel.ab@gmail.com>
2024-09-03 22:05:07 -0500

    google set up complete

---
+++ src/config/passportConfig.js
@@ -0,0 +1,26 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:8000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, done) {
+      // Handle user profile here
+      return done(null, profile);
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+module.exports = passport; // Export the configured passport instance
